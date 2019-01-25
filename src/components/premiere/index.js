819000bfbde3b19703d7256709a459cc220e2a30
Soraya SM <soraya.sacaan@gmail.com>
2019-01-25 17:58:17 -0300

    ejemplo de api

--- src/components/premiere/index.js
+++ src/components/premiere/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
- const Ranked = ({quake}) => (
+ const Premiere = ({quake}) => (
    <div className="quake">
     <h2>{quake.properties.place}</h2>
     <p>Magnitud: {quake.properties.mag} - Profundida: {quake.geometry.coordinates[2]} km</p>
   </div>
  );
 
- export default Ranked;
+ export default Premiere;
