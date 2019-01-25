4330ab77d559824a0ccb04dc9a1be1f20de5f459
Soraya SM <soraya.sacaan@gmail.com>
2019-01-25 16:13:15 -0300

    tdd

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <p>hola</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
