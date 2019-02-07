23980193626e768ff05669d1f6191d884da7761d
Soraya SM <soraya.sacaan@gmail.com>
2019-02-07 12:25:21 -0300

    genres works

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,83 +1,147 @@
 import React, { Component } from 'react';
+import { Dropdown } from "react-bootstrap";
 import './styles.css';
 // importing components
-import Navbar from '../../components/Navbar/index';
 import MovieRow from '../../components/MovieRow/index';
 import Favs from '../../components/Favs/index';
 import Footer from '../../components/Footer/index';
-// API
-const urlPremier= 'https://api.themoviedb.org/3/movie/upcoming?api_key=756e1622851086c3d011b8461693b962&language=es-ES';
-const urlTrend = 'https://api.themoviedb.org/3/trending/all/day?&api_key=756e1622851086c3d011b8461693b962&language=es-ES';
-const urlGenres = 'https://api.themoviedb.org/3/genre/movie/list?api_key=756e1622851086c3d011b8461693b962&language=es-ES';
 // Home Component
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: false,
-      moviesP: [],
-      moviesT: [],
+      premiere: [],
+      trending: [],
       moviesF: [],
       genres: [],
-      filter:[],
       error: false,
     };
   };
-
-// Calling the movie data
-  async componentDidMount() {
-    try {
-      this.setState({loading: true, error: false });
-      // fetch Premiere
-      const responseP = await fetch(urlPremier);
-      const responseJsonP = await responseP.json();
-      const moviesP = responseJsonP.results.slice(0,12);
-      // fetch Trend
-      const responseT = await fetch(urlTrend);
-      const responseJsonT= await responseT.json();
-      const moviesT = responseJsonT.results.slice(0,12); 
-      //States
-      this.setState({moviesP, moviesT, loading: false, error: false });
-    } catch(e) {
-      this.setState({ loading: false, error: true })
-    }
-  };
   // Fetch Genres
   fetchGenres() {
-    fetch(urlGenres)
+    fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=756e1622851086c3d011b8461693b962&language=es-ES')
       .then(response => response.json())
       .then(json => this.setState({ genres: json.genres }));
   };
+  // Fetch Premiere movies
+  fetchPremiere(genres, ranking) {
+    let url = 'https://api.themoviedb.org/3/discover/movie?api_key=756e1622851086c3d011b8461693b962&language=es-ES&primary_release_year=2019';
+    if (genres) {
+      url += "&with_genres=" + genres;
+    }
+    if (ranking) {
+      console.log('lee')
+      url += "&vote_average.gte=" + ranking;
+      url += "&vote_average.lte=" + ++ranking;
+    }
+    return fetch(url);
+  }
+  // Fetch Trending movies
+  fetchTrending(genres, ranking) {
+    let url = 'https://api.themoviedb.org/3/discover/movie?api_key=756e1622851086c3d011b8461693b962&language=es-ES&sort_by=popularity.desc';
+    if (genres) {
+      url += "&with_genres=" + genres;
+    }
+    if (ranking) {
+      console.log('lee')
+      url += "&vote_average.gte=" + ranking;
+      url += "&vote_average.lte=" + ++ranking;
+    }
+    return fetch(url);
+  }
+// All together
+  async componentDidMount(genres, ranking) {
+    try {
+      this.fetchPremiere(genres, ranking)
+        .then(response => response.json())
+        .then(json => this.setState({ premiere: json.results.slice(0, 12) }));
+      this.fetchTrending(genres, ranking)
+        .then(response => response.json())
+        .then(json => this.setState({ trending: json.results.slice(0, 6) }));
+      this.setState({ranking, loading: false, error: false });
+    } catch(e) {
+      this.setState({ loading: false, error: true })
+    }
+  }
+  // Calling genres
   componentWillMount(){
     this.fetchGenres();
   };
+  // filter movies
+  changeGenre(genres) {
+    this.componentDidMount(genres);
+  }
+  changeRanking(ranking) {
+    this.componentDidMount(ranking);
+}
   // Render
  render() {
-  const { moviesP, moviesT, genres, loading, error } = this.state;
+  const { premiere,trending, genres, loading, error } = this.state;
     return (
       <div className="Home">
         <div className="container-flex">
-          <Navbar genres={genres}  /> 
-          <div className="col-12 anchor" id="premiere">
-              <h1>Estrenos</h1>
-              <div className="row">
-                <div className="col-12 text-left">
+        <nav className="navbar navbar-expand-lg navbar-expand-xl navbar-dark bg-danger fixed-top">
+              <div className="container">
+                <a className="navbar-brand" href="/#top">React Movies</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                  <ul className="navbar-nav ml-auto">
+                    <li className="nav-item active">
+                      <a className="nav-link" href="/#top"><i className="fas fa-home"></i> Inicio</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/#premiere"><i className="fas fa-ticket-alt"></i> Estrenos</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/#trend"><i className="fas fa-medal"></i> Más populares</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/#favs"><i className="fas fa-star"></i> Favoritas</a>
+                    </li>
+                    <li>
+                      <Dropdown key={genres.id}>
+                        <Dropdown.Toggle variant="danger" id="dropdown-basic"> Géneros </Dropdown.Toggle>
+                          <Dropdown.Menu> {genres.map(genre=> (
+                            <Dropdown.Item eventKey={genre.id}  key={genre.id}  onSelect={genre => this.changeGenre(genre)}> {genre.name} </Dropdown.Item> ))}
+                          </Dropdown.Menu>
+                        </Dropdown>
+                      </li>
+                      <li>
+                        <Dropdown>
+                          <Dropdown.Toggle variant="danger" id="dropdown-basic"> Ordenar por Nota </Dropdown.Toggle>
+                          <Dropdown.Menu>  {[1,2,3,4,5,6,7,8,9].map(ranking => (  
+                            <Dropdown.Item  eventKey={ranking} key={ranking} onSelect={ranking => this.changeRanking(ranking)} > {ranking}
+                            </Dropdown.Item> ))}
+                            </Dropdown.Menu>
+                          </Dropdown>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-                    {!loading && moviesP.map(movie => <MovieRow movie ={movie} />)}
+              </nav>        
+            <div className="col-12 anchor" id="premiere">
+                <h1>Estrenos</h1>
+                <div className="row">
+                  <div className="col-12 text-left">
+                  </div>
+                      {!loading && premiere.map(movie => <MovieRow movie ={movie} key={movie.id}/>)}
+                      {loading && <div className="col-12 text-center"> <p>Cargando información...</p> </div> }
+                      {!loading && !error && !premiere.length&& <div className="col-12 text-center"> <h2>No hay información disponible.</h2></div> }
+                      {!loading && error && <div className="col-12 text-center"> <h2>Ocurrió un error.</h2></div> }
+                </div>
+              </div>
+            <div className="col-12 anchor" id="trend">
+                <h1>Películas más populares</h1>
+                <div className="row">
+                    {!loading && trending.map(movie =><MovieRow movie ={movie} key={movie.id}/> )}
                     {loading && <div className="col-12 text-center"> <p>Cargando información...</p> </div> }
-                    {!loading && !error && !moviesP.length&& <div className="col-12 text-center"> <h2>No hay información disponible.</h2></div> }
-                    {!loading && error && <div className="col-12 text-center"> <h2>Ocurrió un error.</h2></div> }
-              </div>
+                    {!loading && !error && !trending.length &&  <div className="col-12 text-center"><h2>No hay información disponible.</h2> </div> }
+                    {!loading && error &&  <div className="col-12 text-center"> <h2>Ocurrió un error.</h2> </div>}
+                </div>
             </div>
-          <div className="col-12 anchor" id="Trend">
-              <h1>Películas más populares</h1>
-              <div className="row">
-                  {!loading && moviesT.map(movie =><MovieRow movie ={movie} key={movie.id}/> )}
-                  {loading && <div className="col-12 text-center"> <p>Cargando información...</p> </div> }
-                  {!loading && !error && !moviesT.length &&  <div className="col-12 text-center"><h2>No hay información disponible.</h2> </div> }
-                  {!loading && error &&  <div className="col-12 text-center"> <h2>Ocurrió un error.</h2> </div>}
-              </div>
-          </div>
           <Favs />    
         </div>
         <Footer />   
--- src/screens/Single/index.js
+++ src/screens/Single/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 // Importing Components
-import Navbar from '../../components/Navbar/index';
 import Footer from '../../components/Footer/index';
 //  Single Component
 class Single extends Component {
@@ -52,7 +51,30 @@ class Single extends Component {
     const { movie, loading, error } = this.state;
         return (
             <div>
-                <Navbar />
+                 <nav className="navbar navbar-expand-lg navbar-expand-xl navbar-dark bg-danger fixed-top">
+              <div className="container">
+                <a className="navbar-brand" href="/#top">React Movies</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                  <ul className="navbar-nav ml-auto">
+                    <li className="nav-item active">
+                      <a className="nav-link" href="/#top"><i className="fas fa-home"></i> Inicio</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/#premiere"><i className="fas fa-ticket-alt"></i> Estrenos</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/#trend"><i className="fas fa-medal"></i> Más populares</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/#favs"><i className="fas fa-star"></i> Favoritas</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>        
                 {!loading &&  !error && movie.id &&
                     <div>
                         <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 float-left p-2 my-2 text-center">
